2f860c43c0f4e73c2dc2dabf95e5fb06f699c142
kiritozevs <43684272+kiritozevs@users.noreply.github.com>
2019-10-08 17:10:17 +0300

    yes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+$(document).ready(function(){
+
     $(function() {
         $('.chart').easyPieChart({
             //your options goes here
         });
+
+        $('a[href^="#"]').click(function(){
+        	var target = $(this).attr('href');
+        	$('html, body').animate({
+        		scrollTop: $(target).offset().top
+        	}, 900)
+        });
     });
+});
